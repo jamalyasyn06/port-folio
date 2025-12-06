a20a528eef256644412a19f4b76597b9a8557601
jamal <jamalyasyn06@gmail.com>
2025-12-06 15:33:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+javascript
+// 1.عرض رسالة ترحيبية
+function greet() {
+  alert("أهلاً وسهلاً بك في موقعي!");
+}
+
+// 2. تغيير لون الخلفية
+function changeColor() {
+    document.body.style.backgroundimage = "#cde0e2ff";
+}
+
+
+
+
+
